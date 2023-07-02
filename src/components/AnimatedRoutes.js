512542500721022c0d9e0587b9972f28dc5c9680
IT21156656 <gayashandeshapriya1996219@gmail.com>
2023-07-03 00:14:17 +0530

    set path
    set path

--- src/components/AnimatedRoutes.js
+++ src/components/AnimatedRoutes.js
@@ -1,25 +1,20 @@
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import About from "../pages/about/About";
 import Portfolio from "../pages/portfolio/Portfolio";
 import Contact from "../pages/contact/Contact";
 import Home from "../pages/Home/home";
-import Skills from "../pages/Skills/Skills";
+import  Skills  from "../pages/Skills/Skills";
 import Certify from "../pages/certification/certifi";
+
+
+
 
 const AnimatedRoutes = ({ personalDetails }) => {
   const location = useLocation();
 
-  useEffect(() => {
-    // Redirect to the home page when the user visits the root URL
-    if (location.pathname === "/") {
-      return <Navigate to="/home" replace />;
-    }
-  }, [location]);
-
   return (
     <Routes location={location} key={location.pathname}>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route
         path="/about"
         element={
@@ -37,15 +32,11 @@ const AnimatedRoutes = ({ personalDetails }) => {
       <Route path="/certificate" element={<Certify />} />
 
       <Route path="/portfolio" element={<Portfolio />} />
-
+      
       <Route
         path="/contact"
         element={
-          <Contact
-            name={personalDetails.name}
-            location={personalDetails.location}
-            email={personalDetails.email}
-          />
+          <Contact name={personalDetails.name} location={personalDetails.location} email={personalDetails.email} />
         }
       />
     </Routes>
